571976442c1584aaab882c65c44f0a6ec4f2dc60
Nbddyy <95341178+Nbddyy@users.noreply.github.com>
2023-08-23 21:12:41 +0800

    Create VGA_Plus.v

---
+++ 0822/VGA_Plus/rtl/VGA_Plus.v
@@ -0,0 +1,26 @@
+module VGA_Plus (
+	input clk,
+	input rst_n,
+	output hsync,
+	output vsync,
+	output [15:0] rgb
+);
+
+	wire c0;
+	wire locked;
+
+	PLL_25MHz inst_PLL_25MHz (
+		.areset(!rst_n),
+		.inclk0(clk),
+		.c0(c0),
+		.locked(locked));
+
+	VGA_Pro inst_VGA_Pro (
+		.clk(c0),
+		.rst_n(rst_n && locked),
+		.hsync(hsync),
+		.vsync(vsync),
+		.rgb(rgb)
+	);
+
+endmodule

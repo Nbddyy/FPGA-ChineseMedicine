4d026bc19e77bf67ce21f2b788072b7f92c0af75
Nbddyy <95341178+Nbddyy@users.noreply.github.com>
2023-08-26 16:10:35 +0800

    Create RS232.v

---
+++ 0825/RS232/rtl/RS232.v
@@ -0,0 +1,28 @@
+module RS232 (
+	input clk,
+	input rst_n,
+	input rx,
+	output tx
+);
+
+	wire [7:0] op_data;
+	wire op_flag;
+
+	UART_Rx inst_UART_Rx (
+		.clk(clk),
+		.rst_n(rst_n),
+		.rx(rx),
+		.op_data(op_data),
+		.op_flag(op_flag)
+	);
+
+	UART_Tx inst_UART_Tx (
+		.clk(clk),
+		.rst_n(rst_n),
+		.ip_data(op_data),
+		.ip_flag(op_flag),
+		.tx(tx)
+	);
+
+	
+endmodule
